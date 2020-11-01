4b6612aee5ff73b3606c22323ff3c91b0a7e31e1
Rahul Kanna <67679039+RahulKanna05@users.noreply.github.com>
2020-11-01 17:30:23 +0530

    includes a new guessed_number function and same changes in the start_game function

--- number-guessing.py
+++ number-guessing.py
@@ -1,14 +1,24 @@
 import random
-chances = 5
-attempts=[]
-#number to be choosen
-def number():
+
+#number that the player has to guess
+def number_selection():
     num = random.randint(1,10)
     return num
+
+#guessed number
+def guessed_number():
+    return int(input('guess the number(between 1 and 10):'))
+
 #game
-def game():
-    sltd_num = number()
-    choose_number = int(input('guess the number(between 1 and 10):'))
-    if choose_number<1 and choose_number>10:
-        print('Chose a valid number between 1 and 10')
-game()
+def start_game():
+    chances = 5
+    attempts = []
+    sltd_num = number_selection()
+    choose_number = guessed_number()
+    if choose_number<1 or choose_number>10:
+        print('Choose a valid number between 1 and 10')
+        choose_number = guessed_number()
+    elif choose_number == sltd_num:
+        chances-=1
+        print('Hurray, you have guessed the number')
+
